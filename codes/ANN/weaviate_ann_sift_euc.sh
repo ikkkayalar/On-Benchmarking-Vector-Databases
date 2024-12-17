9536d4f061381963847f50cd6c64b885fe475b9b
ikkkayalar <62773436+ikkkayalar@users.noreply.github.com>
2024-12-17 23:37:50 +0100

    Update weaviate_ann_sift_euc.sh

--- codes/ANN/weaviate_ann_sift_euc.sh
+++ codes/ANN/weaviate_ann_sift_euc.sh
@@ -4,31 +4,36 @@ cpu_limit="8"
 port_mapping_1="8080:8080"
 port_mapping_2="50051:50051"
 weaviate_image="cr.weaviate.io/semitechnologies/weaviate:1.27.1"
+stats_file="weaviate_ann_sift_euc_docker_stats_$(date +"%Y%m%d_%H%M%S").log"
 
-# Weaviate container’ını başlat
+# Start the Weaviate container
 docker run -d --name "$container_name" -p $port_mapping_1 -p $port_mapping_2 --memory="$memory_limit" --cpus="$cpu_limit" "$weaviate_image"
 
-# Container ID’yi container ismine göre yakala
+# Get the container ID based on its name
 container_id=$(docker ps -aqf "name=$container_name")
 
-# Portları ayarla ve .env dosyasına yaz
+# Set up ports and write to .env file
 container_port=$(docker port "$container_id" | grep "8080/tcp" | awk '{print $NF}' | cut -d ':' -f 2)
 cp .env .env.bk
 echo "WEAVIATE_PORT=\"$container_port\"" >> .env
 
-# Weaviate için Python script’ini çalıştır
-python3.10 weaviate_ann_sift_euc.py &
-
-# Python script’inin PID’ini yakala
+# Start the Python script
+python3 weaviate_ann_sift_euc.py &
 python_pid=$!
 
-# Python script’i çalışırken CPU ve bellek kullanımını izle
+# Create a stats file with a header
+echo "Saving Docker stats to $stats_file..."
+echo "CPU%, MemoryUsage, Timestamp" > "$stats_file"
+
+# Monitor CPU and memory usage while the Python script is running
 while ps -p $python_pid > /dev/null; do
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-    docker stats "$container_id" --no-stream --format "{{.CPUPerc}},{{.MemUsage}},$timestamp"
+    docker stats "$container_id" --no-stream --format "{{.CPUPerc}},{{.MemUsage}},$timestamp" >> "$stats_file"
     sleep 5
 done
 
-# Script bitince container’ı durdur ve sil
+# Stop and remove the container after the script ends
 docker rm -f "$container_id"
 mv .env.bk .env
+
+echo "Docker stats saved to $stats_file."
